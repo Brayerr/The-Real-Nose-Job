78a974532a617dd36381657c89485b4288fc95f9
Ellie2405 <mttne2405@gmail.com>
2025-01-24 20:38:48 +0200

    Update BubbleCreator.cs

--- Assets/Scripts/BubbleCreator.cs
+++ Assets/Scripts/BubbleCreator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BubbleCreator : MonoBehaviour
 {
     [SerializeField] Bubble bubblePrefab;
+    [SerializeField] Transform spawnLocation;
     Bubble current;
     float size = 0;
 
@@ -13,14 +14,14 @@ public class BubbleCreator : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            current = Instantiate(bubblePrefab,transform.position,Quaternion.identity);
+            current = Instantiate(bubblePrefab, spawnLocation.position, Quaternion.identity);
         }
         if (Input.GetKey(KeyCode.Space))
         {
             size += Time.deltaTime;
             current.Expand(Time.deltaTime);
         }
-        if(Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             current.Release(size);
             size = 0;
